643e7bc60952da448764bbe878529fec820b5596
Admin <admin@MacBook-Pro-Admin-2.local>
2019-05-09 05:17:56 +0300

    Updates

---
+++ precache-manifest.16dbb7d77513def3b399402ba55c85db.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87abd89654ab68764945736beddedf9c",
+    "url": "/DB2/index.html"
+  },
+  {
+    "revision": "6714030bba992f7734fd",
+    "url": "/DB2/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5600416bc451432fbea8",
+    "url": "/DB2/static/css/main.51118b3f.chunk.css"
+  },
+  {
+    "revision": "6714030bba992f7734fd",
+    "url": "/DB2/static/js/2.3fb49619.chunk.js"
+  },
+  {
+    "revision": "5600416bc451432fbea8",
+    "url": "/DB2/static/js/main.9eb5880e.chunk.js"
+  },
+  {
+    "revision": "76eb1729b423bb9c0a62",
+    "url": "/DB2/static/js/runtime~main.0bebce02.js"
+  },
+  {
+    "revision": "277afe192b265feb21e0923b6b5513cb",
+    "url": "/DB2/static/media/IAP34240.277afe19.png"
+  },
+  {
+    "revision": "e46bbac516bb4fccdbe3598df978096f",
+    "url": "/DB2/static/media/Savin-NY-Website-Background-Web.e46bbac5.jpg"
+  },
+  {
+    "revision": "7556e4d129e2653e55a774635cf25a87",
+    "url": "/DB2/static/media/ada673119a41a7538cc192c2df75f8e2.7556e4d1.gif"
+  },
+  {
+    "revision": "357a6e86d9977ea512d0c31dcd594b4f",
+    "url": "/DB2/static/media/bg2.357a6e86.jpg"
+  },
+  {
+    "revision": "a0ecd676a05d5d9875c19c5c2be9649a",
+    "url": "/DB2/static/media/pxVePU.a0ecd676.gif"
+  },
+  {
+    "revision": "85fe7cd026729c7c0155d9672fc0eeff",
+    "url": "/DB2/static/media/y5ymCvd.85fe7cd0.gif"
+  }
+]);
